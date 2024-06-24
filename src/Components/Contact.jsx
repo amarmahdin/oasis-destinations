51675e951d3b1235d5cc43ef10f020e409a8d21d
MUHAMMAD AMAR MAHDIN <amar2131043@itpln.ac.id>
2024-06-24 13:32:44 +0700

    Slider

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 function Contact() {
   return (
-    <div className='h-[600px]'>
-        <h1 className='text-E font-semibold text-center text-[24px] py-20'>UMPAN BALIK KEPUASAN</h1>
-        <div className=''>
+    <div className='-translate-y-[300px]'>
+        <h1 className='text-E font-semibold text-center text-[24px]'>UMPAN BALIK KEPUASAN</h1>
+        <div className='translate-y-16'>
             <form className='flex'>
-                <div className=' px-48'>
+                <div className='px-48'>
                     <label htmlFor="name" className='mx-3 block text-E text-sm font-semibold mb-1'>Full Name*</label>
                     <input type="text" id='name' name='name' className='shadow-xl w-[526px] h-[60px] px-4 py-2 border-2 border-E rounded-md outline-none' placeholder='Enter your full name here' required />
                     <label htmlFor="Email" className='mx-3 block text-E text-sm font-semibold mb-1 my-6'>Email*</label>
--- src/Components/Content.jsx
+++ src/Components/Content.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
 import tabuan from '../Assets/tabuan.png'
 import loc from '../Assets/loc.png'
 import date from '../Assets/date.png'
@@ -8,13 +10,19 @@ import ticket from '../Assets/ticket.png'
 import tips from '../Assets/tips.png'
 import news from '../Assets/news.png'
 import shape from '../Assets/shape.png'
+import locw from '../Assets/locw.png'
+import im1 from '../Assets/im1.png'
+import im2 from '../Assets/im2.png'
+import im3 from '../Assets/im3.png'
+import im4 from '../Assets/im4.png'
+import im5 from '../Assets/im5.png'
 
 function Content() {
   return (
-    <div className=''>
-      <div className='bg-gradient-to-l from-slate-600/5 to-blue-900/70 h-[1154px] w-[808.12px] absolute '>
+    <div className='-translate-y-[415px]'>
+      <div className='bg-gradient-to-l from-slate-600/5 to-blue-900/70 h-[1163px] w-[808.12px] translate-y-[413px] absolute '>
     <div className='-translate-y-16 flex absolute bg-white h-[135px] left-[250px] w-[1100px] border-2 rounded-[25px]'>
-      <div className='w-[199px] mx-11  flex py-5'>
+      <div className='w-[199px] mx-11 flex py-5'>
         <div className='py-9'>
         <img src={loc} alt="" className='w-5 h-6 '/>
         </div>
@@ -61,16 +69,97 @@ function Content() {
     </form>
     </div>
     </div>
-    <div className=' absolute h-[447px] my-96 mx-14  flex '>
-      <div className='overflow-auto px-2 w-[1000px]'>
-<h1>hahahahah</h1>
-      </div>
-      <div className='overflow-auto px-3 '>
-<h1>hahahahah</h1>
-      </div>
-      <div className='overflow-auto'>
-<h1>hahahahah</h1>
-      </div>
+    <div>
+    <Swiper
+    className='translate-y-[900px]'
+      spaceBetween={50}
+      slidesPerView={3}
+      onSlideChange={() => console.log('slide change')}
+      onSwiper={(swiper) => console.log(swiper)}
+    >
+      <SwiperSlide>
+        <div className='w-[500px] h-[413px] ml-[114px]'>
+          <div className='flex mb-4'>
+            <img src={locw} alt="" className='w-[30px]'/>
+            <h1 className='text-white ml-3'>Banyuwangi, Jawa Timur</h1>
+          </div>
+          <h1 className='text-white font-bold text-[60px] w-[100px]'>PULAU TABUHAN</h1>
+          <p className='text-white w-[450px] mt-6'>Rasakan puncak layanan luar biasa di pulau terbaik dunia pada musim panas ini</p>
+          <div className='flex'>
+            <button className='w-[212px] h-[44px] bg-white text-E border-2 rounded-[40px] mt-10'>Kunjungi</button>
+            <svg xmlns="http://www.w3.org/2000/svg" className='mt-8 ml-6' width="46" height="49" viewBox="0 0 46 49" fill="none">
+  <circle cx="23" cy="26" r="23" fill="white"/>
+  <path d="M23 36.35L21.55 35.05C19.8667 33.5333 18.475 32.225 17.375 31.125C16.275 30.025 15.4 29.0373 14.75 28.162C14.1 27.2867 13.646 26.4827 13.388 25.75C13.13 25.0173 13.0007 24.2673 13 23.5C13 21.9333 13.525 20.625 14.575 19.575C15.625 18.525 16.9333 18 18.5 18C19.3667 18 20.1917 18.1833 20.975 18.55C21.7583 18.9167 22.4333 19.4333 23 20.1C23.5667 19.4333 24.2417 18.9167 25.025 18.55C25.8083 18.1833 26.6333 18 27.5 18C29.0667 18 30.375 18.525 31.425 19.575C32.475 20.625 33 21.9333 33 23.5C33 24.2667 32.871 25.0167 32.613 25.75C32.355 26.4833 31.9007 27.2873 31.25 28.162C30.5993 29.0367 29.7243 30.0243 28.625 31.125C27.5257 32.2257 26.134 33.534 24.45 35.05L23 36.35Z" fill="#27738E"/>
+</svg>
+          </div>
+        </div>
+      </SwiperSlide>
+      <SwiperSlide>
+        <div className='w-[321px] h-[390px] ml-[130px]'>
+          <a href=""><img src={im1} alt="" className='w-[100%] h-[100%] rounded-2xl'/></a>
+          <div className='w-5 h-5 bg-white text-center text-E rounded-[50px] float-end -translate-y-[387px] mr-2'>1</div>
+          <div className='absolute -translate-y-36 ml-8'>          
+            <div className='flex mb-1'>
+            <img src={locw} alt="" className='w-[15px]'/>
+            <h1 className='text-white ml-2 text-[14px]'>Banyuwangi, Jawa Timur</h1>
+          </div>
+          <h1 className='text-white font-bold text-[29px] w-10'>PULAU MENJANGAN</h1>
+          </div>
+        </div>
+      </SwiperSlide>
+      <SwiperSlide>
+      <div className='w-[321px] h-[390px]'>
+        <a href=""><img src={im2} alt="" className=' rounded-2xl'/></a>
+          <div className='w-5 h-5 bg-white text-center text-E rounded-[50px] float-end -translate-y-[387px] mr-2'>2</div>
+          <div className='absolute -translate-y-36 ml-8'>          
+            <div className='flex mb-1'>
+            <img src={locw} alt="" className='w-[15px]'/>
+            <h1 className='text-white ml-2 text-[14px]'>Jember, Jawa Timur</h1>
+          </div>
+          <h1 className='text-white font-bold text-[29px] w-10'>PANTAI PAPUMA</h1>
+          </div>
+        </div>
+      </SwiperSlide>
+      <SwiperSlide>
+      <div className='w-[321px] h-[390px] -translate-x-[130px]'>
+          <a href=""><img src={im3} alt="" className='w-[100%] h-[100%] rounded-2xl'/></a>
+          <div className='w-5 h-5 bg-white text-center text-E rounded-[50px] float-end -translate-y-[387px] mr-2'>3</div>
+          <div className='absolute -translate-y-36 ml-8'>          
+            <div className='flex mb-1'>
+            <img src={locw} alt="" className='w-[15px]'/>
+            <h1 className='text-white ml-2 text-[14px]'>Pacitan, Jawa Timur</h1>
+          </div>
+          <h1 className='text-white font-bold text-[29px] w-10'>PULAU</h1>
+          </div>
+        </div>
+      </SwiperSlide>
+      <SwiperSlide>
+      <div className='w-[321px] h-[390px] -translate-x-[250px]'>
+          <a href=""><img src={im4} alt="" className='w-[100%] h-[100%] rounded-2xl'/></a>
+          <div className='w-5 h-5 bg-white text-center text-E rounded-[50px] float-end -translate-y-[387px] mr-2'>1</div>
+          <div className='absolute -translate-y-36 ml-8'>          
+            <div className='flex mb-1'>
+            <img src={locw} alt="" className='w-[15px]'/>
+            <h1 className='text-white ml-2 text-[14px]'>Banyuwangi, Jawa Timur</h1>
+          </div>
+          <h1 className='text-white font-bold text-[29px] w-10'>PULAU MENJANGAN</h1>
+          </div>
+        </div>
+      </SwiperSlide>
+      <SwiperSlide>
+      <div className='w-[321px] h-[390px] -translate-x-[250px]'>
+          <a href=""><img src={im5} alt="" className='w-[100%] h-[100%] rounded-2xl'/></a>
+          <div className='w-5 h-5 bg-white text-center text-E rounded-[50px] float-end -translate-y-[387px] mr-2'>1</div>
+          <div className='absolute -translate-y-36 ml-8'>          
+            <div className='flex mb-1'>
+            <img src={locw} alt="" className='w-[15px]'/>
+            <h1 className='text-white ml-2 text-[14px]'>Banyuwangi, Jawa Timur</h1>
+          </div>
+          <h1 className='text-white font-bold text-[29px] w-10'>PULAU MENJANGAN</h1>
+          </div>
+        </div>
+      </SwiperSlide>
+    </Swiper>
     </div>
         <img src={tabuan} alt="" className='w-[100%]'/>
         <div className='bg-[#3D809829] h-[300px] flex'>
@@ -93,7 +182,6 @@ function Content() {
             <h1 className='text-E font-medium text-lg text-center'>Tip & Info Perjalanan</h1>
             <p className='text-E text-sm text-center'>Petualangan Menanti! Dapatkan Tips & Info Traveling Terbaru di Website Kami!</p>
             </div>
-            
           </div>
           <div className=' w-[400px] h-[211px] translate-y-9 grid justify-items-stretch translate-x-[200px]'>
             <div className='justify-self-center py-[38px]'>
